73d39c5560252301a9ebe842c338eeb73104a3b6
dtafe <phuc_ceh@hotmail.com>
2018-03-22 02:32:07 -0600

    navbar
    navbar

--- Shop.Web/ClientApp/app/components/app/app.component.ts
+++ Shop.Web/ClientApp/app/components/app/app.component.ts
@@ -1,24 +1,23 @@
-import { Component } from '@angular/core';
-//import * as $ from 'jquery';
-
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.min.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
     public ngOnInit() {
-        //$(document).ready(function () {
-        //    $(".dropdown").hover(
-        //        function () {
-        //            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
-        //            $(this).toggleClass('open');
-        //        },
-        //        function () {
-        //            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
-        //            $(this).toggleClass('open');
-        //        }
-        //    );
-        //});
+        $(document).ready(function () {
+            $(".dropdown").hover(
+                function () {
+                    $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+                    $(this).toggleClass('open');
+                },
+                function () {
+                    $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+                    $(this).toggleClass('open');
+                }
+            );
+        });
     }
 }
