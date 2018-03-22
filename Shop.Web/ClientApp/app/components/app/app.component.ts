2edb2f9d4b9617c8cf03a6a6fb5125688264e8b2
dtafe <phuc_ceh@hotmail.com>
2018-03-21 22:54:49 -0600

    third commit
    third commit

--- Shop.Web/ClientApp/app/components/app/app.component.ts
+++ Shop.Web/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+//import * as $ from 'jquery';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.min.css']
 })
-export class AppComponent {
+export class AppComponent{
+    public ngOnInit() {
+        //$(document).ready(function () {
+        //    $(".dropdown").hover(
+        //        function () {
+        //            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+        //            $(this).toggleClass('open');
+        //        },
+        //        function () {
+        //            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+        //            $(this).toggleClass('open');
+        //        }
+        //    );
+        //});
+    }
 }
